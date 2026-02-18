9fe06f04ecb51145a8fe5fd21fed71e19dd916dd
merock2024 <merock2024@gmail.com>
2026-02-18 14:55:59 +0530

    Added the UC2 feature of asterisk art for printing.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,35 @@
 /*
-OOPS banner App UC1 - Banner Display application
+OOPS banner App UC2 - Print ASCII OOPS Pattern.
 
 This demonstrates a simple java application that displays the Object oriented programming
-system OOPS acronym to console.
+system OOPS Pattern.
 
 */
 
 public class OOPSBannerApp{
 	public static void main(String[] args) {
-		System.out.println("OOPS OOPS");
+		System.out.println("""
+		                                                                      
+      * ***          * ***            ***** **          *******       
+    *  ****        *  ****         ******  ****       *       ***     
+   *  *  ***      *  *  ***       **   *  *  ***     *         **     
+  *  **   ***    *  **   ***     *    *  *    ***    **        *      
+ *  ***    ***  *  ***    ***        *  *      **     ***             
+**   **     ** **   **     **       ** **      **    ** ***           
+**   **     ** **   **     **       ** **      **     *** ***         
+**   **     ** **   **     **     **** **      *        *** ***       
+**   **     ** **   **     **    * *** **     *           *** ***     
+**   **     ** **   **     **       ** *******              ** ***    
+ **  **     **  **  **     **       ** ******                ** **    
+  ** *      *    ** *      *        ** **                     * *     
+   ***     *      ***     *         ** **           ***        *      
+    *******        *******          ** **          *  *********       
+      ***            ***       **   ** **         *     *****         
+                              ***   *  *          *                   
+                               ***    *            **                 
+                                ******                                
+                                  ***                                 
+                                                                      
+		""");
 	}
 }
