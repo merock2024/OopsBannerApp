9f048c48279221cbaf27d8269d3173d82019e4a6
merock2024 <merock2024@gmail.com>
2026-02-18 14:45:43 +0530

    Added the UC1 feature to the app.

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/*
+OOPS banner App UC1 - Banner Display application
+
+This demonstrates a simple java application that displays the Object oriented programming
+system OOPS acronym to console.
+
+*/
+
+public class OOPSBannerApp{
+	public static void main(String[] args) {
+		System.out.println("OOPS OOPS");
+	}
+}
